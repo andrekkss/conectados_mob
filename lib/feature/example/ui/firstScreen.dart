b8208e759efbebc908516d37a70577cfbf493e0f
Kitano <andre.kitano.silva@gmail.com>
2020-03-09 22:11:49 -0300

    Added routes and screens for test

---
+++ lib/feature/example/ui/firstScreen.dart
@@ -0,0 +1,23 @@
+
+import 'package:conectados_mob/Translations.dart';
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(getString(context, "main_title")),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Launch screen'),
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/second');
+          },
+        ),
+      ),
+    );
+  }
+}
